8ad852cca21d4a8352153878792c37c653455ce6
Jefferson Brandão <jeffersonvideo125@gmail.com>
2025-02-06 18:50:29 -0300

    73. Criando a validação dos dados na criação e edição de users

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+    const app = await NestFactory.create(AppModule);
+    app.useGlobalPipes(
+        new ValidationPipe({
+            whitelist: true, // Define uma lista com as propriedades que são permitidas - DTO
+            forbidNonWhitelisted: true, // Bloqueia a requisição caso tenha propriedades não permitidas - DTO
+            transform: true, // Transforma os dados de uma requisção conforme o DTO.
+        }),
+    );
+    await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
